e49207faedc9bb2937f85b591afee22dfd1fe760
Termux School <81871657+Dev-Brasil-Code@users.noreply.github.com>
2021-10-15 12:44:27 -0300

    Add files via upload

---
+++ Kuki.js
@@ -0,0 +1,20 @@
+const Slimbot = require('slimbot');
+const slimbot = new Slimbot(process.env.token);
+const Kukibot  =  require("./Kuki.js");
+const kukiai  =  new  Kukibot({name: (process.env.name), owner: (process.env.owner)});
+
+
+
+slimbot.on('message', message => {
+var text = message.text
+  kukiai.moe(text)
+ .then(reply => {
+  slimbot.sendMessage(message.chat.id, reply);
+ })
+if (message.text.toLowerCase()=='/start'){
+        slimbot.sendMessage(message.chat.id, "Olá me chamo Raissa");
+    }
+});
+
+
+slimbot.startPolling();
